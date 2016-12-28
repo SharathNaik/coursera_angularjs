cb4651ce2d727c0478c73ea620c23e7453341eeb
SharathNaik <nayak.sharathm@gmail.com>
2016-12-28 22:33:18 +0530

    completed functionality

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ angular.module('LunchCheck',[])
 .controller('LunchCheckController',function($scope){
 
 var comma = ',';
+$scope.lunchmenu = '';
 
-function validateLunch(){
-	var result = $scope.lunchmenu.split(comma);
-	alert(result.length);
-}
+$scope.validateLunch =  function(){
+	
+	$scope.message = "";
+	var result ="";
+
+	if($scope.lunchmenu !=""){
+		result = $scope.lunchmenu.split(comma);
+	}
+
+
+	if(result.length == 0){
+		$scope.message = "Please enter data first";
+	}
+	else if(result.length <=3){
+		$scope.message = "Enjoy!";
+	}else if(result.length >=4){
+		$scope.message = "Too much!";
+	}
+};
 
 });
 
